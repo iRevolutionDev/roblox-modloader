b011aa6895d473553fb311df81a5ab1bcd506f43
Revolution <tavinho.daniel@gmail.com>
2025-07-18 17:30:18 -0300

    fix(ci): proxy dll in wrong path

--- scripts/luau.cmake
+++ scripts/luau.cmake
@@ -1,6 +1,11 @@
 include(FetchContent)
 
 message("Luau")
+
+set(LUAU_BUILD_CLI OFF CACHE BOOL "Build Luau CLI" FORCE)
+set(LUAU_BUILD_TESTS OFF CACHE BOOL "Build Luau tests" FORCE)
+set(LUAU_BUILD_WEB OFF CACHE BOOL "Build Luau web components" FORCE)
+
 FetchContent_Declare(
         luau
         GIT_REPOSITORY https://github.com/luau-lang/luau
@@ -8,6 +13,3 @@ FetchContent_Declare(
         GIT_PROGRESS TRUE
 )
 FetchContent_MakeAvailable(luau)
-
-set(LUAU_BUILD_CLI OFF)
-set(LUAU_BUILD_TESTS OFF)
