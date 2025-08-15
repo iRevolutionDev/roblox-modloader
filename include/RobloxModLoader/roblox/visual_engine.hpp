57a78ce5e3560decb7d06a6fc6133e96a6eaf310
Revolution <tavinho.daniel@gmail.com>
2025-08-15 10:26:10 -0300

    fix: adjust padding sizes and add new class members in VisualEngine

--- include/RobloxModLoader/roblox/visual_engine.hpp
+++ include/RobloxModLoader/roblox/visual_engine.hpp
@@ -6,13 +6,13 @@ class VertexStreamer;
 
 class VisualEngine {
 private:
-    char pad_0000[0xA8];
+    char pad_0000[0x90];
 
 public:
     Device *device;
 
 private:
-    char pad_00B0[0x670];
+    char pad_00B0[0x668];
 
 public:
     uintptr_t* dataModel;
@@ -25,4 +25,56 @@ private:
 public:
     float width;
     float height;
+
+private:
+	std::byte pad_0750[0x10];
+
+public:
+	class SceneUpdater* sceneUpdater;
+
+private:
+    std::byte pad_0761[0x10];
+
+public:
+	class TextureCompositor* textureCompositor;
+	class TextureManager* textureManager;
+   
+private:
+	std::byte pad_0768[0x18];
+
+public:
+    class ShaderManager* shaderManager;
+
+private:
+	std::byte pad_0780[0x40];
+
+public:
+	class UITextureRenderer* uiTextureRenderer;
+
+private:
+	std::byte pad_07C0[0x40];
+
+public:
+	class RenderView* renderView;
+	class MeshContentProvider* meshContentProvider;
+	class ContentProvider* contentProvider;
+	class Lighting* lighting;
+	
+private:
+	std::byte pad_0800[0x50];
+
+public:
+	class LightGridUnified* lightGridUnified;
+
+private:
+	std::byte pad_0850[0x18];
+
+public:
+	class TextureAtlas* textureAtlas;
+
+private:
+	std::byte pad_0870[0x20];
+
+public:
+	class ShadowMapSystem* shadowMapSystem;
 };
