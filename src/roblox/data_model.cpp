fb36d10613b833a0426e0f46f45db25e51bd4e00
Revolution <tavinho.daniel@gmail.com>
2025-09-21 22:24:36 -0300

    fix: update to latest roblox version 0.691.0.6910867

--- src/roblox/data_model.cpp
+++ src/roblox/data_model.cpp
@@ -5,7 +5,7 @@
 namespace RBX {
     DataModelType DataModel::get_type() const {
         return static_cast<DataModelType>(*reinterpret_cast<uint32_t *>(
-            reinterpret_cast<std::uintptr_t>(this) + 0x328));
+            reinterpret_cast<std::uintptr_t>(this) + 0x338));
     }
 
     bool DataModel::is_initialized() const {
