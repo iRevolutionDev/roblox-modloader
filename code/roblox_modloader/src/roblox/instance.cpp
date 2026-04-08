1320a11dca179af60575ac32b4d0cce34799bdc2
Revolution <tavinho.daniel@gmail.com>
2026-04-08 02:12:18 -0300

    fix(instance): temporarily return empty string in get_full_name method

--- code/roblox_modloader/src/roblox/instance.cpp
+++ code/roblox_modloader/src/roblox/instance.cpp
@@ -7,6 +7,6 @@ namespace RBX
 	std::string Instance::get_full_name()
 	{
 		uintptr_t result = 0;
-		return get_descriptor().find_function_descriptor("GetFullName")->invoke<std::string&>(this, &result);
+		return "";
 	}
 }
