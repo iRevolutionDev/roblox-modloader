8c3b8412df01a92b86be3a46dabbcf2fa6da2dc8
Revolution <tavinho.daniel@gmail.com>
2026-06-24 15:26:19 -0300

    fix: update PolyHook_2 configuration for improved content fetching

--- cmake/polyhooks.cmake
+++ cmake/polyhooks.cmake
@@ -5,5 +5,8 @@ set(POLYHOOK_BUILD_STATIC_RUNTIME OFF CACHE BOOL "Use dynamic runtime for PolyHo
 FetchContent_Declare(
         polyhook2
         GIT_REPOSITORY https://github.com/stevemk14ebr/PolyHook_2_0.git
+        GIT_TAG f4aee8e47383825469f924903357038b2efd8ca7
+        GIT_SHALLOW TRUE
+        UPDATE_DISCONNECTED TRUE
 )
 FetchContent_MakeAvailable(polyhook2)
