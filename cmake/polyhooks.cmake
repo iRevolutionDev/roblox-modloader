80a1d6e2f39dc1331264866dfd072a891cf321c4
Revolution <tavinho.daniel@gmail.com>
2026-06-30 20:05:01 -0300

    chore(cmake): switch to CPM for PolyHook_2 package management

--- cmake/polyhooks.cmake
+++ cmake/polyhooks.cmake
@@ -1,12 +1,9 @@
-include(FetchContent)
+include("${ROBLOX_MODLOADER_ROOT_DIR}/cmake/cpm.cmake")
 
-set(POLYHOOK_BUILD_STATIC_RUNTIME OFF CACHE BOOL "Use dynamic runtime for PolyHook_2" FORCE)
-
-FetchContent_Declare(
-        polyhook2
-        GIT_REPOSITORY https://github.com/stevemk14ebr/PolyHook_2_0.git
+CPMAddPackage(
+        NAME polyhook2
+        GITHUB_REPOSITORY stevemk14ebr/PolyHook_2_0
         GIT_TAG 49a95d4566d47342b122303cf73585cf22653b0a
-        GIT_SHALLOW TRUE
-        UPDATE_DISCONNECTED TRUE
+        OPTIONS
+        "POLYHOOK_BUILD_STATIC_RUNTIME OFF"
 )
-FetchContent_MakeAvailable(polyhook2)
