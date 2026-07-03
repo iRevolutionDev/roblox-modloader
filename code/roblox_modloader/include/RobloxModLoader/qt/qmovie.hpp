a9c53fc17b1bb80542a2e7735a82d8a15ffadd3e
Revolution <tavinho.daniel@gmail.com>
2026-07-03 20:09:07 -0300

    feat: implement QMovie class for animated image handling in Qt

---
+++ code/roblox_modloader/include/RobloxModLoader/qt/qmovie.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "RobloxModLoader/qt/qobject.hpp"
+#include "RobloxModLoader/qt/qpixmap.hpp"
+
+#include <functional>
+
+namespace rml::qt
+{
+	class QString;
+	
+	class RML_EXPORT QMovie : public QObject
+	{
+	public:
+		enum class CacheMode
+		{
+			None = 0,
+			All = 1,
+		};
+
+		[[nodiscard]] static QMovie* create(const QString& file_name, QObject* parent = nullptr);
+
+		static void destroy(QMovie* movie);
+
+		[[nodiscard]] bool isValid() const;
+		[[nodiscard]] int frameCount() const;
+		[[nodiscard]] int currentFrameNumber() const;
+		[[nodiscard]] int nextFrameDelay() const;
+
+		[[nodiscard]] QPixmap currentPixmap() const;
+
+		void setCacheMode(CacheMode mode);
+		void setSpeed(int percent);
+
+		void start();
+		void stop();
+		void setPaused(bool paused);
+		bool jumpToNextFrame();
+
+		void on_frame_changed(std::function<void()> handler) const;
+	};
+}
---
+++ code/roblox_modloader/src/qt/qmovie.cpp
@@ -0,0 +1,131 @@
+#include "RobloxModLoader/qt/qmovie.hpp"
+
+#include "RobloxModLoader/qt/qstring.hpp"
+#include "RobloxModLoader/qt/qt_module.hpp"
+#include "qt_connect.hpp"
+
+#include <utility>
+
+namespace rml::qt
+{
+	namespace
+	{
+		constexpr std::size_t INSTANCE_SIZE = 128;
+	}
+
+	QMovie* QMovie::create(const QString& file_name, QObject* parent)
+	{
+		static const auto construct = detail::gui<void (*)(void*, void*)>("??0QMovie@@QEAA@PEAVQObject@@@Z");
+		if (!construct)
+			return nullptr;
+
+		void* memory = ::operator new(INSTANCE_SIZE);
+		construct(memory, parent);
+		auto* const movie = static_cast<QMovie*>(memory);
+
+		static const auto set_file_name = detail::gui<void (*)(void*, const void*)>("?setFileName@QMovie@@QEAAXAEBVQString@@@Z");
+		if (set_file_name)
+			set_file_name(movie, file_name.data());
+
+		return movie;
+	}
+
+	void QMovie::destroy(QMovie* movie)
+	{
+		if (!movie)
+			return;
+
+		static const auto dtor = detail::gui<void (*)(void*)>("??1QMovie@@UEAA@XZ");
+		if (dtor)
+			dtor(movie);
+		::operator delete(movie);
+	}
+
+	bool QMovie::isValid() const
+	{
+		static const auto fn = detail::gui<bool (*)(const void*)>("?isValid@QMovie@@QEBA_NXZ");
+		return fn && fn(this);
+	}
+
+	int QMovie::frameCount() const
+	{
+		static const auto fn = detail::gui<int (*)(const void*)>("?frameCount@QMovie@@QEBAHXZ");
+		return fn ? fn(this) : 0;
+	}
+
+	int QMovie::currentFrameNumber() const
+	{
+		static const auto fn = detail::gui<int (*)(const void*)>("?currentFrameNumber@QMovie@@QEBAHXZ");
+		return fn ? fn(this) : 0;
+	}
+
+	int QMovie::nextFrameDelay() const
+	{
+		static const auto fn = detail::gui<int (*)(const void*)>("?nextFrameDelay@QMovie@@QEBAHXZ");
+		return fn ? fn(this) : 0;
+	}
+
+	QPixmap QMovie::currentPixmap() const
+	{
+		static const auto fn = detail::gui<void (*)(const void* self, void* sret)>("?currentPixmap@QMovie@@QEBA?AVQPixmap@@XZ");
+
+		QPixmap result;
+		if (!fn)
+			return result;
+
+		fn(this, result.m_storage);
+		result.m_loaded = true;
+		return result;
+	}
+
+	void QMovie::setCacheMode(const CacheMode mode)
+	{
+		static const auto fn = detail::gui<void (*)(void*, int)>("?setCacheMode@QMovie@@QEAAXW4CacheMode@1@@Z");
+		if (fn)
+			fn(this, static_cast<int>(mode));
+	}
+
+	void QMovie::setSpeed(const int percent)
+	{
+		static const auto fn = detail::gui<void (*)(void*, int)>("?setSpeed@QMovie@@QEAAXH@Z");
+		if (fn)
+			fn(this, percent);
+	}
+
+	void QMovie::start()
+	{
+		static const auto fn = detail::gui<void (*)(void*)>("?start@QMovie@@QEAAXXZ");
+		if (fn)
+			fn(this);
+	}
+
+	void QMovie::stop()
+	{
+		static const auto fn = detail::gui<void (*)(void*)>("?stop@QMovie@@QEAAXXZ");
+		if (fn)
+			fn(this);
+	}
+
+	void QMovie::setPaused(const bool paused)
+	{
+		static const auto fn = detail::gui<void (*)(void*, bool)>("?setPaused@QMovie@@QEAAX_N@Z");
+		if (fn)
+			fn(this, paused);
+	}
+
+	bool QMovie::jumpToNextFrame()
+	{
+		static const auto fn = detail::gui<bool (*)(void*)>("?jumpToNextFrame@QMovie@@QEAA_NXZ");
+		return fn && fn(this);
+	}
+
+	void QMovie::on_frame_changed(std::function<void()> handler) const
+	{
+		static void* const signal = detail::gui_export("?frameChanged@QMovie@@QEAAXH@Z");
+		static const void* const meta = detail::gui_export("?staticMetaObject@QMovie@@2UQMetaObject@@B");
+		detail::connect_function(this, signal, meta, [handler = std::move(handler)](void**) {
+			if (handler)
+				handler();
+		});
+	}
+}
