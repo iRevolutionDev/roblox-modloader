89d5dd472fde5ded0b673fbaffaff21baf4d0c28
Revolution <tavinho.daniel@gmail.com>
2026-07-20 10:09:39 -0300

    feat(macos): add RML_EXPORT to memory management functions for improved visibility

--- code/roblox_modloader/include/RobloxModLoader/platform/memory/host_image.hpp
+++ code/roblox_modloader/include/RobloxModLoader/platform/memory/host_image.hpp
@@ -1,14 +1,16 @@
 #pragma once
 
+#include "RobloxModLoader/rml_export.hpp"
+
 #include <cstdint>
 #include <filesystem>
 #include <string_view>
 
 namespace rml::platform
 {
-	[[nodiscard]] std::filesystem::path module_path_containing(const void* address);
-	[[nodiscard]] std::filesystem::path executable_path();
-	[[nodiscard]] std::string_view studio_image_name();
-	[[nodiscard]] std::uintptr_t studio_preferred_image_base();
-	[[nodiscard]] void* acquire_main_window();
+	[[nodiscard]] RML_EXPORT std::filesystem::path module_path_containing(const void* address);
+	[[nodiscard]] RML_EXPORT std::filesystem::path executable_path();
+	[[nodiscard]] RML_EXPORT std::string_view studio_image_name();
+	[[nodiscard]] RML_EXPORT std::uintptr_t studio_preferred_image_base();
+	[[nodiscard]] RML_EXPORT void* acquire_main_window();
 }
--- code/roblox_modloader/include/RobloxModLoader/platform/memory/memory_protection.hpp
+++ code/roblox_modloader/include/RobloxModLoader/platform/memory/memory_protection.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "RobloxModLoader/rml_export.hpp"
+
 #include <cstddef>
 #include <expected>
 #include <system_error>
@@ -15,6 +17,6 @@ namespace rml::utils
 
 namespace rml::platform
 {
-	[[nodiscard]] std::expected<unsigned long, std::error_code> set_protection(void* address, std::size_t size, utils::MemoryProtection protection);
-	void restore_protection(void* address, std::size_t size, unsigned long previous) noexcept;
+	[[nodiscard]] RML_EXPORT std::expected<unsigned long, std::error_code> set_protection(void* address, std::size_t size, utils::MemoryProtection protection);
+	RML_EXPORT void restore_protection(void* address, std::size_t size, unsigned long previous) noexcept;
 }
