3112c344ebdd5624e7750b8b1dd3ed0840907a77
Revolution <tavinho.daniel@gmail.com>
2026-07-28 12:58:47 -0300

    refactor(dumper): keep only the anchors the loader needs

--- code/tools/dumper/include/rml/dumper/target/anchor.hpp
+++ code/tools/dumper/include/rml/dumper/target/anchor.hpp
@@ -15,21 +15,12 @@ namespace rml::dumper::target
 		lua_settop,
 		lua_resume,
 		luaM_free,
-		luaM_visitgco,
-		luaC_enumheap,
+		luaF_findupval,
 		luaH_new,
-		luaH_setnum,
-		setnodevector,
-		lua_setmetatable,
-		lua_getreadonly,
-		lua_objlen,
 		luaF_newLclosure,
 		luaF_newCclosure,
 		luaF_freeproto,
-		luaF_findupval,
 		luaU_load,
-		lua_pushnumber,
-		lua_toboolean,
 		count,
 	};
 
@@ -45,21 +36,12 @@ namespace rml::dumper::target
 		case Anchor::lua_settop: return "lua_settop";
 		case Anchor::lua_resume: return "lua_resume";
 		case Anchor::luaM_free: return "luaM_free";
-		case Anchor::luaM_visitgco: return "luaM_visitgco";
-		case Anchor::luaC_enumheap: return "luaC_enumheap";
+		case Anchor::luaF_findupval: return "luaF_findupval";
 		case Anchor::luaH_new: return "luaH_new";
-		case Anchor::luaH_setnum: return "luaH_setnum";
-		case Anchor::setnodevector: return "setnodevector";
-		case Anchor::lua_setmetatable: return "lua_setmetatable";
-		case Anchor::lua_getreadonly: return "lua_getreadonly";
-		case Anchor::lua_objlen: return "lua_objlen";
 		case Anchor::luaF_newLclosure: return "luaF_newLclosure";
 		case Anchor::luaF_newCclosure: return "luaF_newCclosure";
 		case Anchor::luaF_freeproto: return "luaF_freeproto";
-		case Anchor::luaF_findupval: return "luaF_findupval";
 		case Anchor::luaU_load: return "luaU_load";
-		case Anchor::lua_pushnumber: return "lua_pushnumber";
-		case Anchor::lua_toboolean: return "lua_toboolean";
 		case Anchor::count: return "count";
 		}
 		return "unknown";
